818cbe4aeff26967475f589ee495ff694a024bbe
khanzzirfan <khanzz_irfan@hotmail.com>
2023-07-15 19:12:29 +1000

    chore: updated gsap with chakra ui interface

--- src/providers/ContextBridge.jsx
+++ src/providers/ContextBridge.jsx
@@ -1,12 +1,16 @@
 // your Stage:
 import { Stage as PixiStage } from "@pixi/react";
 import React from "react";
+import { Flex, Icon, Text, Box, Button } from "@chakra-ui/react";
 import * as PIXI from "pixi.js";
-
+import { ChakraProvider } from "@chakra-ui/react";
 import {
   GsapPixieContext,
   GsapPixieContextProvider,
 } from "./GsapPixieContextProvider";
+
+import { VideoSeekBar } from "./VideoSeekBar";
+
 // the context bridge:
 const ContextBridge = ({ children, Context, render }) => {
   return (
@@ -32,17 +36,28 @@ const Stage = ({ children, ...props }) => {
 export const App = ({ children, backgroundColor, ...props }) => {
   const backgroundColorx = PIXI.utils.string2hex(backgroundColor || "#2D2E3C");
   const width = 600;
-  const height = 600;
+  const height = 500;
 
   /// color: 0x1099bb,
   return (
-    <Stage
-      width={width}
-      height={height}
-      // options={{ background: backgroundColor }}
-      options={{ backgroundColor: backgroundColorx, resolution: 2 }}
-    >
-      {children}
-    </Stage>
+    <div className="App">
+      <ChakraProvider>
+        <Flex flexDir={"column"}>
+          <Stage
+            width={width}
+            height={height}
+            // options={{ background: backgroundColor }}
+            options={{ backgroundColor: backgroundColorx, resolution: 2 }}
+          >
+            {children}
+          </Stage>
+          <GsapPixieContextProvider>
+            <Box mt={5} w={600}>
+              <VideoSeekBar />
+            </Box>
+          </GsapPixieContextProvider>
+        </Flex>
+      </ChakraProvider>
+    </div>
   );
 };
