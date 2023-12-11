5f9436cb824b03063f04ece1a488d2f72be19c9d
Irfan Khan <irfan@infopower.co.nz>
2023-12-12 07:28:09 +1100

    fix: netflify toml file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,10 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  staticDirs: [path.resolve(__dirname, "public")],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
